04b06176963d8c2fc0735b855b8a9fa41b72bbd6
ionutb0g <ionut.bogoi00@gmail.com>
2023-02-09 16:10:52 +0200

    [calendar] set some goals

--- apps/calendar/src/routes/+page.svelte
+++ apps/calendar/src/routes/+page.svelte
@@ -1,2 +1,9 @@
+<script lang="ts">
+	import Calendar from '$lib/Calendar.svelte';
+</script>
+
 <h1>Welcome to Calendar</h1>
 
+<a href="/features" class="text-blue-500 underline">Features</a>
+
+<Calendar />
